d308017a5a1292ec4f20afe6ecee992fc36ed655
Kip Price <4605379+kipprice@users.noreply.github.com>
2023-05-03 08:31:35 -0400

    add PWA support

--- app/root.tsx
+++ app/root.tsx
@@ -7,28 +7,25 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-
 import stylesheet from "~/tailwind.css";
-
+import { useSWEffect } from "~/utils/client/sw-hook";
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
 ];
-
 export default function App() {
+  useSWEffect();
   return (
     <html lang="en" className="h-full w-full p-0 m-0">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <title>Lexical Meditation</title>
         <Meta />
-        <Links />
+        <link rel="manifest" href="/resources/manifest.webmanifest" /> <Links />
       </head>
       <body className="h-full w-full p-0 m-0">
         <div className="left-0 top-0 fixed -z-10 w-screen h-screen bg-paper opacity-40" />
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
+        <Outlet /> <ScrollRestoration /> <Scripts /> <LiveReload />
       </body>
     </html>
   );
