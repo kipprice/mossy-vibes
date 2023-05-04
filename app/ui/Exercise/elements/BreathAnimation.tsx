ca8fada2967083d69dc87ff62ca12d6a6d84691c
Kip Price <4605379+kipprice@users.noreply.github.com>
2023-05-04 17:35:43 -0400

    fix breath animation

--- app/ui/Exercise/elements/BreathAnimation.tsx
+++ app/ui/Exercise/elements/BreathAnimation.tsx
@@ -6,7 +6,7 @@ export type BreathAnimationProps = {}
 export const BreathAnimation: React.FC<BreathAnimationProps> = ({}) => {
   return (
     <motion.div className="w-full flex items-center justify-center p-16">
-      <motion.div className="bg-yellow rounded-full w-vw_25 h-vw_25 relative">
+      <motion.div className="bg-yellow rounded-full w-25vw h-25vw relative">
         <motion.div className="absolute border-2 border-yellow animate-breathe w-full h-full rounded-full">
           <motion.div className="w-2 h-2 rounded-full mt-7 bg-yellow" />
         </motion.div>
