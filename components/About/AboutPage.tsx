2f4ef9185eefdeaac567087d7d92518eb1fd438b
Kip Price <4605379+kipprice@users.noreply.github.com>
2023-05-16 11:24:38 -0400

    add social links

--- components/About/AboutPage.tsx
+++ components/About/AboutPage.tsx
@@ -1,23 +1,32 @@
-import React from 'react';
-import { LogFooter, PageHeader } from '../shared';
+import React from "react";
+import { LogFooter, PageHeader } from "../shared";
+import { SocialLinks } from "../shared/SocialLinks";
 
-export type AboutPageProps = {
-    
-};
+export type AboutPageProps = {};
 
-export const AboutPage: React.FC<AboutPageProps> = ({  }) => {
-    return(
-        <>
-        <PageHeader title={'About'} />
-        <div className='flex flex-col gap-8 text-xl font-light py-4 px-6'>
+export const AboutPage: React.FC<AboutPageProps> = ({}) => {
+  return (
+    <>
+      <PageHeader title={"About"} />
+      <div className="flex flex-col gap-8 text-xl font-light py-4 px-6">
         <p>Welcome to Mossy Vibes!</p>
-        <p>We created Mossy Vibes as a way to make it easier to find meditative and mindful moments, no matter where you are mentally or physically. The exercises are short, and designed to be read rather than listened to; and every day, we try to add a new one!</p>
-        <p>We don&apos;t track any of your data, but we do store your preferences directly on your device. This information is only visible to you.</p>
-        <p>If you enjoy using Mossy Vibes and want to support us as we create new content and features, please consider <a href='https://www.buymeacoffee.com/mossyvibes' className='underline font-base bg-off-white bg-opacity-10 rounded-md p-0.5'>donating or becoming a member!</a></p>
-        
-        </div>
+        <p>
+          We created Mossy Vibes as a way to make it easier to find meditative
+          and mindful moments, no matter where you are mentally or physically.
+          The exercises are short, and designed to be read rather than listened
+          to; and every day, we try to add a new one!
+        </p>
+        <p>
+          We don&apos;t track any of your data, but we do store your preferences
+          directly on your device. This information is only visible to you.
+        </p>
+        <SocialLinks
+          content="If you enjoy using Mossy Vibes and want to help us keep it going, we appreciate any support you are able to give!"
+          className="text-left"
+        />
+      </div>
 
-        <LogFooter />
-        </>
-    );
+      <LogFooter />
+    </>
+  );
 };
--- components/shared/SocialLinks/SocialLinks.tsx
+++ components/shared/SocialLinks/SocialLinks.tsx
@@ -1,32 +1,46 @@
-import React from "react";
+import React, { HTMLProps } from "react";
 import { SocialLink } from "./SocialLink";
 import { generateShareLinks } from "../../../utils/client/share";
 import FeatherIcon from "feather-icons-react";
 import { Icon } from "../Icon";
-export type SocialLinksProps = {};
+export type SocialLinksProps = Pick<HTMLProps<HTMLDivElement>, "className"> & {
+  content?: string;
+};
 
-export const SocialLinks: React.FC<SocialLinksProps> = ({}) => {
+export const SocialLinks: React.FC<SocialLinksProps> = ({
+  content = "Want to help us keep Mossy Vibes going strong?",
+  className = "text-center",
+}) => {
   const links = generateShareLinks();
 
   return (
     <div className="w-full flex flex-col gap-4 pb-4">
-      <div className="text-center font-light">
-        Want to help us keep Mossy Vibes going strong?
-      </div>
+      <div className={`${className} font-light`}>{content}</div>
       <div className="flex px-8 w-full justify-between items-start">
-        {/* <SocialLink title="Follow">
-          <a href="https://instagram.com/mossy_vibes_app" className="p-2" title='follow us on instagram'>
-            <FeatherIcon icon="instagram" size={24} />
-          </a>
-        </SocialLink> */}
-        <SocialLink title="Share">
-          <a href={links.email} className="p-2" title='share through email'>
+        <SocialLink title="Our Info">
+          <a
+            href={"mailto:hello@mossyvibes.com"}
+            className="p-2"
+            title="contact us through email"
+          >
             <FeatherIcon icon="mail" size={24} />
           </a>
-          <a href={links.facebook} className="p-2" title='share on facebook'>
+          <a
+            href="https://instagram.com/mossy_vibes_app"
+            className="p-2"
+            title="follow us on instagram"
+          >
+            <FeatherIcon icon="instagram" size={24} />
+          </a>
+        </SocialLink>
+        <SocialLink title="Share">
+          <a href={links.email} className="p-2" title="share through email">
+            <FeatherIcon icon="mail" size={24} />
+          </a>
+          <a href={links.facebook} className="p-2" title="share on facebook">
             <FeatherIcon icon="facebook" size={24} />
           </a>
-          <a href={links.twitter} className="p-2" title='share on twitter'>
+          <a href={links.twitter} className="p-2" title="share on twitter">
             <FeatherIcon icon="twitter" size={24} />
           </a>
         </SocialLink>
@@ -34,7 +48,7 @@ export const SocialLinks: React.FC<SocialLinksProps> = ({}) => {
           <a
             className={`text-off-white pt-1`}
             href="https://buymeacoffee.com/mossyvibes"
-            title='donate or subscribe on Buy Me a Coffee'
+            title="donate or subscribe on Buy Me a Coffee"
           >
             <Icon iconFileName="bmc-logo" className="w-8 h-8" />
           </a>
